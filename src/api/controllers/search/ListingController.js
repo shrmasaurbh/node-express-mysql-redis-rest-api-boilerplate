0919a0f91e67d72b53b173995d6d670b920f5133
saurabh <shrmasaurbh@gmail.com>
2020-01-30 16:35:38 +0530

    listing api es

--- src/api/controllers/search/ListingController.js
+++ src/api/controllers/search/ListingController.js
@@ -28,6 +28,10 @@ module.exports = {
     },
 
     async getSearchListing(req,res) {
+        var meta ={
+                    "status": 200,
+                    // "error" : false
+                }
         var postData = req.body;  
         // postData = JSON.parse(postData);
         // console.log("req.params",postData);
@@ -53,33 +57,64 @@ module.exports = {
             if(postData.filters){
                 urlObj.filters = postData.filters[0];
             }
-            const resultData = esModel.getSearchListing(urlObj);
-            // console.log(resultData)
-            return resultData.then(function(result) {
-                var resResponse = {};
-                var resData = JSON.parse(result);
-                // console.log(resData)
-                if (resData['success']) {
-                    // console.log(resData['data'])
-                    var metaData = {};
-                    metaData.status = 200;
-                    metaData.is_error = 0;
-                    metaData.total_records = resData['total'];
-                    metaData.message = resData['message'];
-                    metaData.is_aggregator = resData['is_aggregator'];
-                    metaData.pageId = urlObj.pageId;
-                    metaData.from = urlObj.fromData;
-                    metaData.size = urlObj.size;
-                    metaData.total_page = Math.ceil(resData['total'] / urlObj.size);
-                    resResponse.meta = metaData;
-                    resResponse.data = resData['data'];
-                    resResponse.user_auth_token = '';
+            // const resultData = await SearchDB.getSearchListing(urlObj);
+            try{
+                const resultData = await SearchDB.getSearchListing(urlObj);
+                
+
+                if(resultData.is_error){
+                    meta.message = resultData.message;
+                    return apiResp.apiErr( req, res, 400, meta);
                 }
+
+                var data = [];
+                // console.log(resultData['data']['hits'].length);
+                if(resultData['data']['total']){
+                    
+                    meta.count = resultData['data']['total'];
+            console.log("resultData")
+                    meta.pageId = urlObj.pageId;
+                    meta.from = urlObj.fromData;
+                    meta.size = urlObj.size;
+                    meta.total_page = Math.ceil(meta.count / urlObj.size);
+                    
+                    resultData['data']['hits'].forEach((value) => {
+                                console.log(value['_source']);
+                                data.push(value['_source']);
+                            });
+                    
+                    return apiResp.apiResp( req, res, data, meta );
+                }else{
+                    return apiResp.apiResp( req, res, data, meta );
+                }
+
+            }catch (err) {
+                return apiResp.apiErr( req, res, 400, err);  
+            }
+            // return resultData.then(function(result) {
+            //     var resResponse = {};
+            //     // var resData = JSON.parse(result);
+            //     // console.log(resData)
+            //     if (resData['success']) {
+            //         // console.log(resData['data'])
+            //         var metaData = {};
+            //         metaData.status = 200;
+            //         metaData.is_error = 0;
+            //         metaData.total_records = resData['total'];
+            //         metaData.message = resData['message'];
+            //         metaData.is_aggregator = resData['is_aggregator'];
+            //         metaData.pageId = urlObj.pageId;
+            //         metaData.from = urlObj.fromData;
+            //         metaData.size = urlObj.size;
+            //         metaData.total_page = Math.ceil(resData['total'] / urlObj.size);
+            //         resResponse.meta = metaData;
+            //         resResponse.data = resData['data'];
+            //     }
                 // ctx.set('Access-Control-Allow-Origin', '*');
                 // ctx.set('Content-Type', 'application/json');
                 // ctx.body = JSON.stringify(resResponse)
                 // ctx.body = result;
-            });
+            // });
         }
     },
 
--- src/api/models/SearchModel.js
+++ src/api/models/SearchModel.js
@@ -1,6 +1,6 @@
 const esClient = require('../../config/esconfig');
 const index = "homesfy_search";
-const type = "projects";
+const indexType = "projects";
 
 module.exports = {
 
@@ -12,7 +12,7 @@ module.exports = {
         	const esResponse = await new Promise(function(resolve, reject) { 
 		        esClient.search({
 		            "index": index,
-		            "type": type,
+		            "type": indexType,
 		            "body": {
 		                "query": {
 		                    "bool": {
@@ -63,8 +63,6 @@ module.exports = {
 		    	return {message:msg,is_error:0,data:esResponse['hits']['hits']}
 	        }
 	        
-	        console.log(esResponse, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-
 
       //   }catch (err) {
 
@@ -76,12 +74,13 @@ module.exports = {
       //   }
 
     },
+
     async getDetailsById(id) {
         var err = {	is_error:0};
         try{
 	        const esResponse = await esClient.get({
 	            "index": index,
-	            "type": type,
+	            "type": indexType,
 	            "id": id,
 	            "ignore":404
 	        });
@@ -92,70 +91,106 @@ module.exports = {
 	        }else{
 		    	return {message:"Data Not Found",is_error:1,data:[]}
 	        }
-	        console.log("esResponse",esResponse);
 
         }catch (err) {
-
-        	console.log("err", err);
 		    return {message:err.message,is_error:1,data:[]}
-            //throw error in json response with status 500.
-            // return err;
-
         }
 
     },
 
-    async getSearchListing(queryStr, bhk_filter = null) {
+    async getSearchListing(urlObj) {
 
-        var restResponse = {};
-        var queryString = queryStr.trim().replace(/-/g, "");
+         var restResponse = {};
+        restResponse.message = [];
+        // var queryString = urlObj.query.trim().replace(/[^a-zA-Z0-9\ ]/g, ' ')
+        var queryString = urlObj.query.trim().replace(/-/g, "");
         queryString = queryString.replace(/[()]/g, ' ');
 
-        var filter = [];
+        // var esQuery = '{"must":{"multi_match":{"fields" : ["tags", "name"], "query" : "'+queryString+'", "type": "cross_fields", "analyzer": "standard", "operator": "and", "fuzziness": "AUTO"} },';
+        var esQuery = '{"must":{"multi_match":{"fields" : ["tags", "name"], "query" : "' + queryString.toLowerCase() + '", "analyzer": "standard", "operator": "and"} },';
 
-        filter.push(JSON.parse('{"term": {"is_active": 1 } }'));
 
+        // esQuery += '"filter":{ "bool" : { "must" : [{"term": {"is_active": 1 }},';
+        esQuery += '"filter":{ "bool" : { "must" : [';
+        if (urlObj.filters) {
 
-        if(bhk_filter){
-        	filter.push(JSON.parse('{"term": {"configuration": "'+bhk_filter.toLowerCase()+'" } }'));
-        }
+            if (urlObj.filters.bed_config) {
+                esQuery += '{"terms":{"bed_config":' + JSON.stringify(urlObj.filters.bed_config) + ' }},';
+            }
 
-        const esResponse = await new Promise(function(resolve, reject) { 
-	        esClient.search({
-	            "index": "homesfy_search",
-	            "type": "projects",
-	            "body": {
-	                "query": {
-	                    "bool": {
-	                        "must": {
-	                            "multi_match": {
-	                                "fields": ["tags", "name"],
-	                                "query": queryString,
-	                                "analyzer": "standard",
-	                                "operator": "and"
-	                            }
-	                        },
-			                "filter":{
-								"bool": {
-									"must": filter
-								}
-							}
-	                    }
-	                },
-	            }
-	        },function(error, results, fields) {
-                if (error) {
-                    reject(error);
+            if (urlObj.filters.property_type) {
+                esQuery += '{"terms":{"property_type":' + JSON.stringify(urlObj.filters.property_type) + ' }},';
+            }
+
+            if (urlObj.filters.sale_type) {
+                esQuery += '{"terms":{"sale_type":' + JSON.stringify(urlObj.filters.sale_type) + ' }},';
+            }
+
+            var priceRange = '';
+            if (urlObj.filters.price) {
+                if (urlObj.filters.price.length === 1) {
+                    esQuery += '{"range" : {"price" : {"gte" : ' + urlObj.filters.price[0].from + ', "lte" : ' + urlObj.filters.price[0].to + '}}}';
                 } else {
+                    let firstPrice = urlObj.filters.price[0];
+                    let lastPrice = urlObj.filters.price[urlObj.filters.price.length - 1];
 
-                    resolve(results);
-                    // return results;
+                    esQuery += '{"range" : {"price" : {"gte" : ' + firstPrice.from + ', "lte" : ' + lastPrice.to + '}}}';
                 }
-            });
-	    }).catch(function(err){
-            console.log(err);
-        });
+            }
+        }
+        esQuery += ']}}'
 
-        return JSON.stringify(esResponse['hits']['hits']);
-    },
+        esQuery = esQuery.replace("},]", "}]");
+
+        esQuery += '}'
+        esQuery = esQuery.replace("},}", "}}");
+        console.log("esQuery",esQuery)
+        // try{
+
+	        const esResponse = await new Promise(function(resolve, reject) { 
+	        	esClient.search({
+		            "index": index,
+		            "type": indexType,
+		            "from": urlObj.fromData,
+		            "size": urlObj.size,
+		            "body": {
+		                "query": {
+		                    "bool": JSON.parse(esQuery)
+		                },
+		                // "aggs": {
+		                //     "max_price": { "max": { "field": "price" } },
+		                //     "min_price": { "min": { "field": "price" } },
+		                //     "origin": { "terms": { "field": "type" } },
+		                //     "brands": { "terms": { "field": "brand_id" } },
+		                //     "compatibility": { "terms": { "field": "model_id" } }
+		                // }
+		            }
+		        },function(error, results, fields) {
+	                if (error) {
+	                	console.log("in reject")
+	                    reject(error);
+	                } else {
+	                    resolve(results);
+	                    // return results;
+	                }
+	            });
+		    }).catch(function(err){
+		    	// var err = {	message:err.message,
+			    // 			is_error:1,
+			    // 		}; 
+		    	return {message:err.message,is_error:1,data:[]}
+	        });
+
+        // console.log("esResponse......",esResponse)
+
+	        if(esResponse.is_error){
+	        	console.log("err.message2",esResponse);
+	        	var msg = esResponse.message ? esResponse.message : "No data is found"; 
+		    	return {message:msg,is_error:1,data:[]}
+	        }else{
+		        var msg = esResponse['hits']['total']> 0 ? "success" : "No data is found"; 
+		    	return {message:msg,is_error:0,data:esResponse['hits']}
+	        }
+
+    }
 }
