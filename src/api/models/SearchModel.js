c7bb1ca441c87c6d9a11687eb40da89d0b048fc0
chetankumarVarma <chetan.v@homesfy.in>
2020-02-12 17:34:43 +0530

    test

--- src/api/models/SearchModel.js
+++ src/api/models/SearchModel.js
@@ -1,6 +1,6 @@
 const esClient = require('../../config/esconfig');
-const index = "homesfy_new";
-const indexType = "new_projects";
+const index = "homesfy_search";
+const indexType = "projects";
 
 module.exports = {
 
